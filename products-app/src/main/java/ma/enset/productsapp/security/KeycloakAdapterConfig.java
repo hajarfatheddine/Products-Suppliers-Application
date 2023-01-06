09bd725c5a3ef19345dfdb3923a1c0d30e1de18b
hajar fatheddine <fatheddinehajar@gmail.com>
2023-01-07 00:16:49 +0100

    securing the products-app

---
+++ products-app/src/main/java/ma/enset/productsapp/security/KeycloakAdapterConfig.java
@@ -0,0 +1,14 @@
+package ma.enset.productsapp.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConfig {
+    //it tells tha app to take into consideration the configuration in application.properties instead of keycloak.json
+    @Bean
+    public KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return  new KeycloakSpringBootConfigResolver();
+    }
+}
